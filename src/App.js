09bdce169e24b5db85b274c279a8bace9d7efdaa
kimyoonkyeom <newalpha7@naver.com>
2023-02-05 21:35:03 +0900

    [main] 컴포넌트 구조 변경 & 뒤로가기 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
-import {List} from "./features/list/List";
-import {User} from './features/user/User'
-import {BrowserRouter as Router, Route, useLocation} from 'react-router-dom'
-import {MenuSchema} from './menu/MenuSchema'
 import CustomRoute from './menu/CustomRoute'
+import {useHistory} from 'react-router-dom'
 
 function App() {
+  const history = useHistory()
+  const onBack = () => {
+    history.goBack()
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <CustomRoute />
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <button onClick={onBack}>{'뒤로가기'}</button>
+        <CustomRoute />
+      </header>
+    </div>
   );
 }
 
