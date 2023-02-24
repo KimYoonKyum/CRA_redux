377455ef8c69c11f1332fa7c6aa28f78cdffe6da
YoonKyeom Kim <newalpha7@naver.com>
2023-02-25 00:35:50 +0900

    [main] 스타일 수정

--- src/pages/card-list/CardListPage.js
+++ src/pages/card-list/CardListPage.js
@@ -7,18 +7,15 @@ import {
   selectIsLoading,
   selectList
 } from "../../features/list/listSlice";
-import {Card, CardContent, Skeleton} from "@mui/material";
-import React from "react";
+import {Card, CardContent, Skeleton, SpeedDial, SpeedDialAction, SpeedDialIcon} from "@mui/material";
+import React, {useEffect} from "react";
 import '../../styles/CardListPage.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export function CardListPage() {
   const list = useSelector(selectList)
   const isLoading = useSelector(selectIsLoading)
   const dispatch = useDispatch();
-
-  const onApiCall = () => {
-    dispatch(getCardListAsync())
-  }
 
   const onApiToken = () => {
     dispatch(getTokenAsync())
@@ -27,37 +24,59 @@ export function CardListPage() {
   const renderSkeleton = () => {
     return (
       <>
-        <Skeleton variant="rounded" width={210} height={60} style={{marginTop:10}}/>
-        <Skeleton variant="rounded" width={210} height={60} style={{marginTop:10}}/>
-        <Skeleton variant="rounded" width={210} height={60} style={{marginTop:10}}/>
-        <Skeleton variant="rounded" width={210} height={60} style={{marginTop:10}}/>
+        <Skeleton variant="rounded" width={407} height={558} />
+        <Skeleton variant="rounded" width={407} height={558} />
+        <Skeleton variant="rounded" width={407} height={558} />
+        <Skeleton variant="rounded" width={407} height={558} />
       </>
     )
   }
+
+  const renderCardList = () => {
+    return (
+      <div className={'CardList flex flex-one hbox center wrap'}>
+        {isLoading && renderSkeleton()}
+        {list.map(card=>{
+          return (
+            <Card key={card.id}>
+              <CardContent>
+                <img src={card.image}/>
+              </CardContent>
+            </Card>
+          )
+        })}
+      </div>
+    )
+  }
+
+  const renderFooterDial = () => {
+    return (
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        <SpeedDialAction
+          key={'dial'}
+          icon={<MenuIcon />}
+          tooltipTitle={'menu'}
+        />
+      </SpeedDial>
+    )
+  }
+
+  useEffect(()=>{
+    dispatch(getCardListAsync())
+  },[])
 
   return (
     <div className={'CardListPage flex flex-one center vbox'}>
       <div>{`리스트 개수 : ${list.length}`}</div>
       <div>
-        <button onClick={onApiCall}>{'Api call'}</button>
         <button onClick={onApiToken}>{'Api token call'}</button>
       </div>
-      <div className={'CardList flex vbox'}>
-        {isLoading && renderSkeleton()}
-        {list.map(card=>{
-          return (
-            // <div key={card.id} className={'flex col'}>
-            //   {card.name}
-            //   <img src={card.image} />
-            // </div>
-          <Card>
-            <CardContent>
-              <img src={card.image} />
-            </CardContent>
-          </Card>
-          )
-        })}
-      </div>
+      {renderCardList()}
+      {renderFooterDial()}
     </div>
   )
 }
