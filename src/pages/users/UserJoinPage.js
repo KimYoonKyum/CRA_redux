ade9a79d35a356a999a5bd10f15e0ac401920ff6
YoonKyeom Kim <newalpha7@naver.com>
2023-03-31 19:35:52 +0900

    [main] 간편회원가입 퍼블리싱

--- src/pages/users/UserJoinPage.js
+++ src/pages/users/UserJoinPage.js
@@ -1,9 +1,21 @@
 import React, {useEffect, useRef} from "react";
+import '../../styles/UserJoinPage.css'
+import {Button, Card, TextField} from "@mui/material";
 
 export function UserJoinPage() {
 
   return (
     <div className={'UserJoinPage flex flex-one center vbox'}>
+      <Card className={'card vbox'}>
+        <div className={'title center'}>{'Easy Join'}</div>
+        <div className={'vbox info'}>
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" type={'password'} label="Password" variant="outlined" />
+          <TextField id="outlined-basic" type={'password'} label="Password-Confirm" variant="outlined" />
+        </div>
+
+        <Button className={'join-btn'}>{'Join!'}</Button>
+      </Card>
     </div>
   )
 }
